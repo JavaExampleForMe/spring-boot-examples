3947a5c84a7d52281c6f3f8564bd4ca2d623a0ef
Ida Amit <Ida.Amit@nice.com>
2020-01-18 19:19:32 +0200

    Adding writing to special log Loggersp

--- refresh-logging-level/src/main/java/com/example/refreshlogginglevel/SampleService.java
+++ refresh-logging-level/src/main/java/com/example/refreshlogginglevel/SampleService.java
@@ -1,6 +1,8 @@
 package com.example.refreshlogginglevel;
 
 import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalTime;
@@ -9,12 +11,19 @@ import java.time.format.DateTimeFormatter;
 @Slf4j
 @Service
 public class SampleService {
+    static Logger loggerSp = LoggerFactory.getLogger("storedProcedureDebug");
+
     public String sayHellow() {
 
         log.trace("This is a trace message");
         log.debug("This is a debug message");
         log.error("This is error message");
         log.info("This is info message");
+        if (log.isTraceEnabled()) {
+            log.debug("Trace enabled");
+            loggerSp.trace("this is the trace for the sp");
+        }else
+            log.info("Trace Not enabled");
         return "Hellow was called " + LocalTime.now().format(DateTimeFormatter.ofPattern("HH:mm:ss"));
     }
 
